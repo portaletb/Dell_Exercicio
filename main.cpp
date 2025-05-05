ccbaef5a6f817c13ea4d2d4131c658823af572ae
portaletb <portaletbb@gmail.com>
2025-05-05 23:49:40 +0000

    Atualizações Gerais

--- main.cpp
+++ main.cpp
@@ -1,1 +1,47 @@
 #include "main.hpp"
+using namespace std;
+int main() {
+    vector<Pessoa> jogadores;
+    int qtd;
+
+    cout << "Quantos jogadores deseja cadastrar? ";
+    cin >> qtd;
+    cin.ignore(); // limpar o '\n' do buffer
+
+    for (int i = 0; i < qtd; ++i) {
+        string nome, nick;
+        int pontos;
+
+        cout << "\nJogador " << (i + 1) << ":\n";
+        cout << "Nome completo: ";
+        getline(cin, nome);
+
+        cout << "Nickname: ";
+        getline(cin, nick);
+
+        cout << "Pontuação no ranking: ";
+        cin >> pontos;
+        cin.ignore();  // limpar o buffer após número
+
+        jogadores.emplace_back(nome, nick, pontos);
+    }
+
+    if(qtd <4 || qtd >8 || qtd %2 != 0){
+       cout << "O número de jogadores é inválido";
+    }
+    else {
+
+    // Embaralhar os jogadores
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    shuffle(jogadores.begin(), jogadores.end(), default_random_engine(seed));
+
+    // Executar jogos em pares
+    Torneio t;
+    for (size_t i = 0; i + 1 < jogadores.size(); i += 2) {
+        t.jogo(jogadores[i], jogadores[i + 1]);
+    }
+   
+}
+
+    return 0;
+}
--- main.hpp
+++ main.hpp
@@ -1,44 +1,102 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
 using namespace std;
 
 class Pessoa {
-private:
-    string nome;
-    string nickname;
-    int pontuacaoRanking;
-    int armazenaPonto = 70;
-
-public:
-    // Construtor com 3 parâmetros
-    Pessoa(string n, string nick, int pontuacao) {
-        nome = n;
-        nickname = nick;
-        pontuacaoRanking = pontuacao;
-    }
-
-    // Getters
-    string getNome() {
-        return nome;
-    }
-
-    string getNickname() {
-        return nickname;
-    }
-
-    int getPontuacaoRanking() {
-        return pontuacaoRanking;
-    }
-};
-
-class Menu {
+    private:
+        string nome;
+        string nickname;
+        int pontuacaoRanking;
+        int pontos = 70;
+    
     public:
-        void exibirMenu() {
-            int opcao;
+        Pessoa(string n, string nick, int pontuacao)
+            : nome(n), nickname(nick), pontuacaoRanking(pontuacao) {}
+    
+        string getNome() {
+            return nome;
+        }
+    
+        string getNickname() {
+            return nickname;
+        }
+    
+        int getPontuacaoRanking() {
+            return pontuacaoRanking;
+        }
 
-cout << "Pressione 1 para Jogada orginal (+5pts)" <<endl;
-cout << "Pressione 2 para Gafe (-3pts)" <<endl;
-cout << "Pressione 3 para Posicionamento vantajoso (+2pts)"<<endl;
-cout << "Pressione 4 para Desrespeito ao adversário (-5pts)"<<endl;
-cout << "Pressione 5 Ataque de fúria (-7pts)"<<endl;
+        void atualizarPontos(int delta) {
+            pontos += delta;
+        }
+    };
+
+    class Torneio {
+        public:
+            void jogo(Pessoa &p1, Pessoa &p2) {
+                cout << "\n=== Jogo entre " << p1.getNickname()
+                     << " e " << p2.getNickname() << " ===\n";
+        
+                int escolhaJogador;
+                cout << "Quem fará a jogada?\n";
+                cout << "1 - " << p1.getNickname() << "\n";
+                cout << "2 - " << p2.getNickname() << "\n";
+                cout << "Escolha (1 ou 2): ";
+                cin >> escolhaJogador;
+        
+                Pessoa* jogadorEscolhido = nullptr;
+        
+                if (escolhaJogador == 1)
+                    jogadorEscolhido = &p1;
+                else if (escolhaJogador == 2)
+                    jogadorEscolhido = &p2;
+                else {
+                    cout << "Opção inválida. Ninguém jogou.\n";
+                    return;
+                }
+        
+                cout << "\nEscolha a ação:\n";
+                cout << "1 - Jogada original (+5pts)\n";
+                cout << "2 - Gafe (-3pts)\n";
+                cout << "3 - Posicionamento vantajoso (+2pts)\n";
+                cout << "4 - Desrespeito ao adversário (-5pts)\n";
+                cout << "5 - Ataque de fúria (-7pts)\n";
+                cout << "Digite sua escolha: ";
+        
+                int acao;
+                cin >> acao;
+        
+                int delta = 0;
+        
+                switch (acao) {
+                    case 1:
+                        delta = 5;
+                        break;
+                    case 2:
+                        delta = -3;
+                        break;
+                    case 3:
+                        delta = 2;
+                        break;
+                    case 4:
+                        delta = -5;
+                        break;
+                    case 5:
+                        delta = -7;
+                        break;
+                    default:
+                        cout << "Ação inválida.\n";
+                        return;
+                }
+        
+                jogadorEscolhido->atualizarPontuacao(delta);
+        
+                cout << jogadorEscolhido->getNickname()
+                     << " agora tem " << jogadorEscolhido->getPontuacaoRanking()
+                     << " pontos no ranking.\n";
+            }
+        };
+        
